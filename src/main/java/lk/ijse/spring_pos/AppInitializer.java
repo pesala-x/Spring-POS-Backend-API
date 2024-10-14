aa446ed05a2e424c6064a6e98e74519e0c446a41
pesala-x <gkpesalawinodith@gmail.com>
2024-10-14 10:53:46 +0530

    Health Test successfully ran.

--- src/main/java/lk/ijse/spring_pos/AppInitializer.java
+++ src/main/java/lk/ijse/spring_pos/AppInitializer.java
@@ -1,20 +1,22 @@
 package lk.ijse.spring_pos;
 
+import lk.ijse.spring_pos.config.WebAppConfig;
+import lk.ijse.spring_pos.config.WebAppRootConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebAppRootConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebAppConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[]{"/"};
     }
 }
