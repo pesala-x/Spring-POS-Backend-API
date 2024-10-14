47db0d7b69049f2e584ae7cf1af034f42ec3079e
pesala-x <gkpesalawinodith@gmail.com>
2024-10-14 23:14:34 +0530

    DateTime util implemented.

---
+++ src/main/java/lk/ijse/spring_pos/util/Date_TimeUtil.java
@@ -0,0 +1,9 @@
+package lk.ijse.spring_pos.util;
+
+import java.time.LocalDateTime;
+
+public class Date_TimeUtil {
+    public static LocalDateTime getCurrentDateTime() {
+        return LocalDateTime.now();
+    }
+}
