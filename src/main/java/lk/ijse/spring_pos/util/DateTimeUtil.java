aaf13b7940e2d293ff4c505adc1894997777a596
pesala-x <gkpesalawinodith@gmail.com>
2024-10-16 20:21:35 +0530

    Item Save(POST Request) function implemented successfully..

--- src/main/java/lk/ijse/spring_pos/util/DateTimeUtil.java
+++ src/main/java/lk/ijse/spring_pos/util/DateTimeUtil.java
@@ -1,9 +1,14 @@
 package lk.ijse.spring_pos.util;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class DateTimeUtil {
     public static LocalDateTime getCurrentDateTime() {
         return LocalDateTime.now();
+    }
+
+    public static LocalDate getCurrentDate() {
+        return LocalDate.now();
     }
 }
